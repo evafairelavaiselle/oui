a2693612a210c5258f5d552772a1637508125be2
evafairelavaiselle <146078439+evafairelavaiselle@users.noreply.github.com>
2023-10-02 15:49:38 +0200

    push

---
+++ ex_08.js
@@ -0,0 +1,9 @@
+if ( breadCount >= 2 && hamCount >=1){
+    displayThisText("i can make a ham sandwich")
+    }
+    if ( breadCount >= 2 && tunaCount >=1){
+    displayThisText("i can make a tuna sandwich")
+    }
+    if ( breadCount >=2 && tunaCount >=1 && hamCount >=1){
+    displayThisTest("i can make a royal sandwich")
+    }
