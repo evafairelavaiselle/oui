d9242b6c2a3d395b5cc317a2fe95310430dfb830
evafairelavaiselle <146078439+evafairelavaiselle@users.noreply.github.com>
2023-10-02 14:13:37 +0200

    push

---
+++ ex_05.js
@@ -0,0 +1,8 @@
+var firstname = "John"
+var lastname = "Doe"
+var cardcod = "1111 2222 3333 4444"
+var expiryDateMonth = 21
+var expiryDateYear = 2042
+var computedBankInfo = (firstname + "," + lastname + "," + cardcod + "," + expiryDateMonth + "/" + expiryDateYear)
+
+displayInCard(computedBankInfo)
---
+++ ex_06.js
@@ -0,0 +1,9 @@
+if (bananasCount == 0){
+    displayThisText("Oh no, there is no banana")
+    }
+    if (bananasCount > 0){
+    displayThisText("Yummy!")
+    }
+    if (bananasCount < 0){
+    displayThisText("Do I owe you bananas?")
+    }
