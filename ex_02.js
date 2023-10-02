36fdeebccf4b7b10ed9012d3bc68d3cff35d311a
evafairelavaiselle <146078439+evafairelavaiselle@users.noreply.github.com>
2023-10-02 11:44:13 +0200

    Update ex_02.js

--- ex_02.js
+++ ex_02.js
@@ -1,1 +1,10 @@
-document.write("Hello World!")
+var integer = 42
+var float = 42.42
+var string = "forty two"
+var bool = true
+var this_is_null
+displayThisText(bool)
+displayThisText(float)
+displayThisText(string)
+displayThisText(integer)
+displayThisText(this_is_null)
